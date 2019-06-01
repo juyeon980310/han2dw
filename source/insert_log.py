3836f1bbdd9e44a649afd3a9a01aad3ce7d5e280
woong232 <woong232@gmail.com>
2019-06-01 20:47:13 +0900

    Change Source

--- source/insert_log.py
+++ source/insert_log.py
@@ -1,25 +1,32 @@
 import pymysql
 
 
-conn = pymysql.connect(host='localhost', user='han2um', password='root',
+conn = pymysql.connect(host='localhost', user='han1um', password='root',
                        db='han2um', charset='utf8')
  
 curs = conn.cursor(pymysql.cursors.DictCursor)
 
-sql = "select tag_num from key_table where tag_id='45f2c6ef'"
+sql = "select tag_num from key_table where tag_id='99734356'"
 curs.execute(sql)
-result = curs.fetchone()
+tagNum = curs.fetchone()
 #print(result['tag_num'])
 
-sql = """insert into log_DB(tag_num,date,time)
-         values (%s, CURRENT_DATE(), CURRENT_TIME())"""
+sql = "select tag_num from current where tag_num=%s"
+curs.execute(sql, (tagNum['tag_num']))
+tagFlag = curs.fetchone()
+print(tagFlag)
 
-curs.execute(sql, (result['tag_num']))
+
+sql = """insert into log_DB(tag_num,date,time)
+			values (%s, CURRENT_DATE(), CURRENT_TIME())"""
+if tagFlag is None:
+	curs.execute(sql, (tagNum['tag_num']))
 
 sql = """insert into current(tag_num,date,time)
          values (%s, CURRENT_DATE(), CURRENT_TIME())"""
 
-curs.execute(sql, (result['tag_num']))
+if tagFlag is None:
+	curs.execute(sql, (tagNum['tag_num']))
 
 conn.commit()
 
